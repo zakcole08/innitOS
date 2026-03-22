88c091b1395cc273fdc1ec91c6473b5b63190f1a
zakcole08 <zakcole08@gmail.com>
2026-03-22 23:14:14 +0000

    Add error handling, better logic and output

--- build.sh
+++ build.sh
@@ -1,10 +1,41 @@
 #!/usr/bin/env bash
 
-mkdir -p rootfs/{proc,sys,bin,sbin}
+# colours
+GRN='\e[32m'
+RED='\e[31m'
+RST='\e[0m'
 
-gcc -static src/innit.c -o src/innit
-gcc -static src/mishell.c -o src/mishell
-mv src/mishell rootfs/bin
-mv src/innit rootfs/sbin
+# vars
+INNIT_SRC="src/innit.c"
+MISHELL_SRC="src/mishell.c"
+INNIT_DST="rootfs/sbin/innit"
+MISHELL_DST="rootfs/bin/mishell"
+
+mkdir -p rootfs/{proc,sys,bin,sbin}
+if [[ $? -ne 0 ]]; then
+  echo -e "${RED}FAILED: create folders: rootfs, rootfs/{proc,sys,bin,sbin}${RST}"
+  exit 1
+else
+  echo -e "${GRN}Created folders: rootfs, rootfs/{proc,sys,bin,sbin}${RST}"
+fi
+
+gcc -static $INNIT_SRC -o $INNIT_DST
+if [[ $? -ne 0 ]]; then
+  echo -e "${RED}FAILED: compile $INNIT_DST${RST}"
+else
+  echo -e "${GRN}Compiled $INNIT_DST${RST}"
+fi
+gcc -static $MISHELL_SRC -o $MISHELL_DST
+if [[ $? -ne 0 ]]; then
+  echo -e "${RED}FAILED: compile $MISHELL_DST${RST}"
+else
+  echo -e "${GRN}Compiled $MISHELL_DST${RST}"
+fi
+
 cd rootfs
 find . | cpio -o -H newc | gzip > ../initramfs.img
+if [[ $? -ne 0 ]]; then
+  echo -e "${RED}FAILED: create initramfs.img${RST}"
+else
+  echo -e "${GRN}Created initramfs.img${RST}"
+fi
