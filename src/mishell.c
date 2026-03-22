2a25e5ecbc3e31a012fdebc03e90b097cef6ad5c
zakcole08 <zakcole08@gmail.com>
2026-03-22 23:42:24 +0000

    Add pwd command

--- src/mishell.c
+++ src/mishell.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
+char *version = "v0.1";
 
 int cmd_help() {
+	printf("innitOS %s\n", version); 
 	printf("help - see this help menu\n");
+	printf("pwd - print the current working directory\n");
+	return 0;
+}
+
+int cmd_pwd() {
+	char buf[1024];
+	getcwd(buf, sizeof(buf));
+	printf("%s\n", buf);
 	return 0;
 }
 
 int main() {
 	char command[65536];
+	printf("innitOS %s\n", version);
 	while (1) {
 		printf("> ");
 		// Ensure input does not exceed buffer size
@@ -24,6 +37,9 @@ int main() {
 		}
 		else if (strcmp(command, "help") == 0) {
 				cmd_help();
+		}
+		else if (strcmp(command, "pwd") == 0) {
+			cmd_pwd();
 		}
 		else {
 			printf("%s does not exist as a command on this system\n", command);
